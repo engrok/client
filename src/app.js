96179bf17a8e24e45bf0e3a66d05861799645b1a
tamar-mepe <tdavi18@freeuni.edu.ge>
2021-08-19 01:28:18 +0400

    add simple client functionality

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 const WebSocket = require('ws');
 const socket = new WebSocket('ws://localhost:8080');
+const axios = require('axios');
+const ip = '127.0.0.1', port = '3000';
 
-socket.on('open', () => {
-    socket.send('something');
-});
-
-socket.onmessage = ({ data }) => {
-    console.log('Message from server ', data);
+socket.onmessage = ({ message }) => {
+    processServerMessage(message);
+    axios.get(`http://${ip}:${port}`)
+        .then(function (response) {
+            let processedRes = processLocalServerResponse(response.headers);
+            socket.send(JSON.stringify(processedRes));
+        })
+        .catch(function (error) {
+            // TODO
+        })
+        .then(function () {
+            // TODO
+        });
 };
+
+function processServerMessage(message) {
+}
+
+function processLocalServerResponse(message) {
+    return message;
+}
