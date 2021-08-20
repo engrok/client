eff8080241a2c315b66ea84f64304a5b4893a866
tamar-mepe <tdavi18@freeuni.edu.ge>
2021-08-21 01:40:01 +0400

    start parsing server message

--- src/app.js
+++ src/app.js
@@ -3,24 +3,37 @@ const socket = new WebSocket('ws://localhost:8080');
 const axios = require('axios');
 const ip = '127.0.0.1', port = '3000';
 
-socket.onmessage = ({ message }) => {
-    processServerMessage(message);
-    axios.get(`http://${ip}:${port}`)
-        .then(function (response) {
-            let processedRes = processLocalServerResponse(response.headers);
-            socket.send(JSON.stringify(processedRes));
-        })
-        .catch(function (error) {
-            // TODO
-        })
-        .then(function () {
-            // TODO
-        });
+socket.onmessage = (event) => {
+    let processedData = processServerMessage(event.data);
+    let neededUrl = processedData.url;
+    let neededHeaders = processedData.headers;
+
+    const config = {
+        method: 'get',
+        url: `http://${ip}:${port}${neededUrl}`,
+        headers: neededHeaders
+    };
+
+    axios(config).then(function (response) {
+        let processedRes = processLocalServerResponse(response);
+        socket.send(JSON.stringify(processedRes));
+    }).catch(function (error) {
+        // TODO
+    }).then(function () {
+        // TODO
+    });
 };
 
-function processServerMessage(message) {
+function processServerMessage(dataArg) {
+    data = JSON.parse(dataArg);
+    let host = data.headers.host;
+    dataArg = dataArg.replace(new RegExp(host, "g"), `${ip}:${port}`);
+    return JSON.parse(dataArg);
 }
 
-function processLocalServerResponse(message) {
-    return message;
+function processLocalServerResponse(response) {
+    return {
+        data: response.data,
+        headers: response.headers
+    };
 }
